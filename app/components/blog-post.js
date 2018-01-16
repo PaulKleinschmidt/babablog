ccda6ceca08bddcd1536ca4051db1ed5f55f6b26
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2018-01-16 18:24:09 -0500

    Adds styling
    forgot to checkout to new branch

--- app/components/blog-post.js
+++ app/components/blog-post.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
       deletePost (post) {
-        console.log(post)
         post.deleteRecord();
         post.save();
       }
