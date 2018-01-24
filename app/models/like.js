dd495a567ce84e0585d966bcd45f9f25677dd005
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2018-01-24 15:33:48 -0500

    User id's of likers shows up next to each post
    - by using {{like.user.email}}, the user's email shows up next to
         each post
    - This is not what I want to happen, but it helps for testing

--- app/models/like.js
+++ app/models/like.js
@@ -4,11 +4,3 @@ export default DS.Model.extend({
   user: DS.belongsTo('user'),
   post: DS.belongsTo('post')
 });
-
-// game: DS.belongsTo('game', {
-//     inverse: 'libraries'
-//   }),
-//   user: DS.belongsTo('user', {
-//     inverse: 'libraries'
-//   })
-// });
