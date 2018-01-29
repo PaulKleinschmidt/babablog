c0c32666e77436463bb12d19c290329193967041
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2018-01-29 16:29:54 -0500

    Adds ability to like posts from app
    User can like post once
    Like button is hidden by using computed property in like-counter.js

--- app/components/like-counter.js
+++ app/components/like-counter.js
@@ -2,10 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   auth: Ember.inject.service(),
+  likedByUser: false,
+  toggleLikedByUser: Ember.computed('likedByUser', function() {
+      this.toggleProperty('likedByUser');
+    }),
   actions: {
     addLike(post) {
       this.sendAction('addLike', post)
       // like.save();
+    },
+    deleteLike(likes) {
+
     }
   }
 });
